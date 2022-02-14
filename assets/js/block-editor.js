26d03f5f39b0989ff1e4eac63552018fb6e65cbf
Jef Ogues <jef.ogu@gmail.com>
2022-02-13 22:14:17 -0500

    Added Paragraph Style

--- assets/js/block-editor.js
+++ assets/js/block-editor.js
@@ -3,6 +3,11 @@ wp.blocks.registerBlockStyle( 'core/quote', {
     label: 'Fancy Quote',
 } );
 
+wp.blocks.registerBlockStyle( 'core/paragraph', {
+    name: 'center-text',
+    label: 'Center',
+} );
+
 wp.domReady( function () {
     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
