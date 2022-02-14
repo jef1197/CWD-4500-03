60e783df4b0a0362937946e83e106729a15dd61a
Jef Ogues <jef.ogu@gmail.com>
2022-02-13 21:43:56 -0500

    First Commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package CWD_4500_03
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+				<?php
+				/* translators: 1: Copyright, 2: Site. */
+				printf( esc_html__( 'Copyright %1$s by %2$s.', 'cwd' ), '2022', '<a href="' . home_url() . '">CWD 4500 03</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
